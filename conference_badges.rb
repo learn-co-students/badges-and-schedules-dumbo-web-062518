165953441792dfb375e9af1ec9b238617b757906
Jane Costa <yberdysh@gmail.com>
2018-06-21 15:31:50 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,12 @@ def assign_rooms(arr)
 end
 
 def printer(attendees)
-	attendees.each do |person|
-		puts batch_badge_creator(person)
-		puts assign_rooms(person)
-	end 
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
+
